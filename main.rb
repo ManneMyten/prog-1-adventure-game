585e8379ef9f47964bfcb7948a840a4a22517f14
ManneMyten <manne.laurin@elev.ga.ntig.se>
2024-04-08 13:44:18 +0200

    add main-loop

--- main.rb
+++ main.rb
@@ -2,5 +2,14 @@
 
 def intro()
     puts "welcome to the world of Dork"
-    puts "You open your eyes and see that you are in a forest"
+    puts "type your actions with 3-word prompts"
+    puts "You open your eyes and see that you are in a forest clearing, in front of a cave entrance"
+end
+
+def main()
+    # intro()
+
+    puts "You are in a corridor with a gate in front of you, and the path continues to you left and right"
+    action = gets.
+
 end
