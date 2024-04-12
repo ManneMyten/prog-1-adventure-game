e284a3ba59da842bd2d63308431d0453cde42775
ManneMyten <manne.laurin@elev.ga.ntig.se>
2024-04-12 11:55:26 +0200

    added player movement and validation

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 $player_coordinates = [0, 2]
 
 #Map kordinater map[row][col]
-map = [
+$map = [
     ["path", "path", "entrance", "path", "path"], 
     ["path", "room1", "path", "room2", "path"], 
     ["path", "room3", "path", "room4", "path"], 
@@ -17,10 +17,24 @@ def intro()
 end
 
 def current_room()
-    $player_room = map[$player_coordinates[0]][$player_coordinates[1]]
+    $player_room = $map[$player_coordinates[0]][$player_coordinates[1]]
+
+    #innehåller rummen omkring spelaren i ordning [vänster, ner, höger]
+    surrounding_rooms = [$map[$player_coordinates[0]][$player_coordinates[1] - 1], $map[$player_coordinates[0] + 1][$player_coordinates[1]], $map[$player_coordinates[0]][$player_coordinates[1] + 1]]
+
 
     if $player_room == "entrance"
         puts "You have 3 corridors around you, one straight forward and one on either side of you. Which way do you go?"
+    
+    elsif $player_room == "path"
+        if $player_coordinates == [1,2]
+            puts "As you walk down the corridor you encounter two doors on either side, while the corridor keeps going. The doors appear to be unlocked."
+        elsif $player_coordinates == [2,2]
+            puts "Further down the dark tunnel you see another set of doors, same as the last ones."
+        end
+
+    elsif $player_room == "room1" #Spindelrum?
+        puts ""
     end
 
 
@@ -30,9 +44,9 @@ def action(action) #Ska uppdatera olika variabler som inventory och coordinater 
     valid_input = false
     
     while valid_input == false
-        if $player_room == "path"
+        if $player_room == "path" || $player_room == "entrance"
             if action[0] == "forward" || action[1] == "forward"
-                if player_coordinates[0] + 1 != "path"
+                if $map[$player_coordinates[0] + 1][$player_coordinates[1]] != "path"
                     valid_input = false
                     puts "There is no way forward, choose another path"
                     action = gets.chomp.split
@@ -40,22 +54,37 @@ def action(action) #Ska uppdatera olika variabler som inventory och coordinater 
                     valid_input = true
                     $player_coordinates[0] += 1
                 end
-                # fortsätt skriva här
+                
+            elsif action[0] == "left" || action[1] == "left"
+                if $map[$player_coordinates[0]][$player_coordinates[1] + 1] != "path" #spelarens vänster är höger på kartan, så måste öka x-värdet
+                    valid_input = false
+                    puts "There is no path to your left, choose another way"
+                    action = gets.chomp.split
+                else
+                    valid_input = true
+                    $player_coordinates[1] += 1
+                end
+
+            elsif action[0] == "right" || action[1] == "right"
+                if $map[$player_coordinates[0]][$player_coordinates[1] - 1] != "path" 
+                    valid_input = false
+                    puts "There is no path to your right, choose another way"
+                    action = gets.chomp.split
+                else
+                    valid_input = true
+                    $player_coordinates[1] -= 1
+                end
             end
-
-        elsif $player_room == "entrance"
-            allowed_actions = ["right", "left", "forward"]
-
+        #elsif $player_room... fler rum
         end
     end
-    #Add validation of input
 
     
 end
 
 def main()
-    # intro()
-    $player_coordinates = [0, 2]
+    #intro()
+    $player_coordinates = [0, 2] #entrance-rummets koordinater
     restart_game = false
 
     while restart_game == false
@@ -66,3 +95,6 @@ def main()
     end
 
 end
+
+
+main()
