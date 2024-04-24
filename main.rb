63932fa539c97a391d15f014c08959cdbfd0f08f
ManneMyten <manne.laurin@elev.ga.ntig.se>
2024-04-24 13:36:17 +0200

    Started work on movement

--- main.rb
+++ main.rb
@@ -1,18 +1,12 @@
 require("./Rooms.rb")
+require("./movement.rb")
 
 $player_coordinates = [0, 2]
 $coins = 3
 $inventory = ["#{$coins} coins", "rusty dagger"]
 $possible_actions = []
 
-#Map kordinater map[row][col]
-$map = [
-    ["path", "path", "entrance", "path", "path", nil],
-    ["path", "room1", "path", "room2", "path", nil],
-    ["path", "room3", "path", "room4", "path", nil],
-    ["room5", "empty", "path", "room6", "path", nil],
-    ["room7", "path", "path", "empty", "room8", nil]
-]
+
 
 def fancy_text(text)
     i =0
---
+++ movement.rb
@@ -0,0 +1,13 @@
+#Map koordinater map[row][col]
+$map = [
+    ["path", "path", "entrance", "path", "path", nil],
+    ["path", "room1", "path", "room2", "path", nil],
+    ["path", "room3", "path", "room4", "path", nil],
+    ["room5", "empty", "path", "room6", "path", nil],
+    ["room7", "path", "path", "empty", "room8", nil]
+]
+
+def move(direction)
+
+end
+
