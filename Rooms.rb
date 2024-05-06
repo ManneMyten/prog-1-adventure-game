7733b38f0b1de8366967ca28de47247c8eb85d86
Erfan-Rahimi <erfan.rahimi@elev.ga.ntig.se>
2024-05-06 13:03:24 +0200

    Update Rooms.rb

--- Rooms.rb
+++ Rooms.rb
@@ -3,13 +3,17 @@ $room2 = ["Entering the room you see a table with a rusty old key on it.\nYou al
 $room3 = []
 $room4 = []
 $room5 = ["Opening the oak door you find, to your surprise, an old man standing behind a desk. \nWould you like to buy something? he utters while smiling warmly. \nWhat will you do?", ["open", "walk", "exit", "leave", "buy", "talk"]]
-
+$room6 = []
+$room7 = []
+$room8 = ["You have found the big red Ruby. You are now the crown winner of NTI Kronhus"]
 #first index = description, second index is possible actions
 rand_room_1 = ["As you walk into the room you are met with a chest positioned upon an intricately designed stone stand,\nbut the ominous feeling of a presence in the dark alerts you.\nA giant spider suddenly appears before you falling from the ceiling!\nWhat do you do?", ["open", "exit", "leave", "attack", "walk"]]
 rand_room_2 = ["Upon entering the room you notice the faint glowing of a torch,\nyou quickly notice the skeleton holding the torch and a sword.What will you do?", ["walk", "exit", "leave", "attack"]]
+rand_room_3 = ["You have found a healing potion. What will you do?", ["open", "exit", "leave", "attack", "walk"]]
+rand_room_4 = ["There is a open chest with 5 healing potions, is your luck unbelievable or does somethin seem sketchy?. What will you do?", ["walk", "exit", "leave", "attack"]]
 
 #array of randomized rooms.
-$roomlist = [rand_room_1, rand_room_2]
+$roomlist = [rand_room_1, rand_room_2, rand_room_3, rand_room_4]
 
 def rooms(room)
 
@@ -17,11 +21,11 @@ def rooms(room)
     if room == "room1"
         return $room1
 
-    elsif room == "room5"
-        return $room5
-
     elsif room == "room2"
         return $room2
+
+    elsif room == "room5"
+        return $room5
 
     elsif room == "room3" && $room3 != []
         return $room3
@@ -29,19 +33,23 @@ def rooms(room)
     elsif room == "room4" && $room4 != []
         return $room4
 
-    elsif room == "room3" || room == "room4"
+    elsif room == "room3" || room == "room4" || room == "room5" || room == "room6"
         randomnum = rand(0..($roomlist.length - 1))
         assignedroom = $roomlist[randomnum]
         $roomlist.delete_at(randomnum)
 
         if room == "room3"
             $room3 = assignedroom
-            $room4 =  $roomlist[0]
             return $room3
         elsif room == "room4"
             $room4 = assignedroom
-            $room3 = $roomlist[0]
             return $room4
+        elsif room == "room6"
+            $room6 = assignedroom
+            return $room6
+        elsif room == "room7"
+            $room7 = assignedroom
+            return $room7
         end
     end
 end
